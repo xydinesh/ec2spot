5f92cda930508d88a1cb5d18d1c3c18324506119
Dinesh Weerapurage <xydinesh@gmail.com>
2012-11-25 04:06:55 +0000

    working on initial release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
 name='EC2Spot',
-version='0.1.1',
+version='0.1.0',
 author='Dinesh P. Weerapurage',
 author_email='xydinesh@gmail.com',
 packages=['ec2spot'],
